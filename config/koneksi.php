c6d52ffa2672adf998f8e70b8030a6d6ca4312ef
fahmi0721 <fahmiidrus131@gmail.com>
2024-01-03 10:26:18 +0800

    update

--- config/koneksi.php
+++ config/koneksi.php
@@ -5,7 +5,7 @@
 	    PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
 	];
 	try{
-		$db=new PDO('mysql:host=localhost;dbname=n1732945_ims','n1732945_ims','nLcd7Tp1Y$Sw',$options);
+		$db=new PDO('mysql:host=localhost;dbname=ims_2024','root','',$options);
 	}catch(PDOException $e) {
 	    die("Database connection failed: " . $e->getMessage());
 	}
