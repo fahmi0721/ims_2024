a497fde667bf23a833bea26428f4b428ad03cbc4
fahmi0721 <fahmiidrus131@gmail.com>
2024-01-12 09:39:00 +0800

    update master divisi

--- config/koneksi.php
+++ config/koneksi.php
@@ -10,16 +10,16 @@
 	//     die("Database connection failed: " . $e->getMessage());
 	// }
  
-	$options = [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_CASE => PDO::CASE_NATURAL,
-		PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
-	];
-	try{
-		$db=new PDO('mysql:host=localhost;dbname=n1732945_ims','n1732945_ims','nLcd7Tp1Y$Sw',$options);
-	}catch(PDOException $e) {
-		die("Database connection failed: " . $e->getMessage());
-	}
+		$options = [
+		    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		    PDO::ATTR_CASE => PDO::CASE_NATURAL,
+		    PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
+		];
+		try{
+			$db=new PDO('mysql:host=localhost;dbname=n1732945_ims','n1732945_ims','nLcd7Tp1Y$Sw',$options);
+		}catch(PDOException $e) {
+		    die("Database connection failed: " . $e->getMessage());
+		}
 ?>
 
 
